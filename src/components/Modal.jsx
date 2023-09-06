defd40b0980334ef1ef1d3910de251d2a8ebc233
otvertka <otvertka123@ukr.net>
2023-09-06 12:11:19 +0300

    animate_presence

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,8 +6,13 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit={{ opacity: 0, y: 30 }}
         open
         className="modal"
       >
